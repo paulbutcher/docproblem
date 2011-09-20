3e65461754a7fdf63c377153722d5a7b65eabd1b
Paul Butcher <paul@paulbutcher.com>
2011-09-20 14:53:41 +0100

    Add dummy project as a workaround

--- project/Build.scala
+++ project/Build.scala
@@ -6,8 +6,10 @@ object DocProblem extends Build {
   override lazy val settings = super.settings ++ Seq(crossScalaVersions := Seq("2.8.1", "2.9.1"))
   
   lazy val root = Project("Root", file(".")) aggregate(core, supplement)
+    
+  lazy val dummy = Project("Dummy", file("dummy"))
   
-  lazy val core = Project("Core", file("core"))
+  lazy val core = Project("Core", file("core")) dependsOn(dummy)
 
   lazy val supplement = Project("Supplement", file("supplement")) dependsOn(core)
 }
